c1a885e4a9851332cd36843b94be3d4ff6b7f9ee
amyyan210 <amy@yanaway.com>
2018-11-17 16:09:59 -0600

    Initial commit

---
+++ Grades/GradeStatistics.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Grades
+{
+    public class GradeStatistics
+    {
+        public GradeStatistics()
+        {
+            HighestGrade = 0;
+            LowestGrade = 100;
+        }
+
+        public float HighestGrade;
+        public float LowestGrade;
+        public float AverageGrade;
+        
+    }
+}
---
+++ Grades/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Gradebook book = new Gradebook();
+            book.AddGrade(31);
+            book.AddGrade(98.7f);
+            book.AddGrade(82.5f);
+
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine("Average grade: " + stats.AverageGrade);
+            Console.WriteLine("Highest grade: " + stats.HighestGrade);
+            Console.WriteLine("Lowest grade: " + stats.LowestGrade);
+        }
+    }
+}
