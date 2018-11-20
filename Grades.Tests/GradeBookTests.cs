6ce2f78167ba7e703597b6bb42f4f2bb67c80837
ayanaway@wbhq.com <ayanaway@wbhq.com>
2018-11-20 14:43:51 -0600

    * Grades.sln: Create unit tests for each gradebook computation
    * UnitTest1.cs:
    * GradeBookTests.cs:
    * Grades.Tests.csproj:

---
+++ Grades.Tests/GradeBookTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Xunit;
+
+namespace Grades.Tests
+{   
+    public class GradeBookTests
+    {
+        [Fact]
+        public void ComputesHighestGrade() 
+        {
+            Gradebook gradebook = new Gradebook();
+            gradebook.AddGrade(10);
+            gradebook.AddGrade(90);
+
+            GradeStatistics result = gradebook.ComputeStatistics();
+            Assert.Equal(90, result.HighestGrade);
+        }
+
+        [Fact]
+        public void ComputesLowestGrade()
+        {
+            Gradebook gradebook = new Gradebook();
+            gradebook.AddGrade(10);
+            gradebook.AddGrade(90);
+
+            GradeStatistics result = gradebook.ComputeStatistics();
+            Assert.Equal(10, result.LowestGrade);
+        }
+
+        [Fact]
+        public void ComputesAverageGrade()
+        {
+            Gradebook gradebook = new Gradebook();
+            gradebook.AddGrade(91);
+            gradebook.AddGrade(89.5f);
+            gradebook.AddGrade(75);
+
+            GradeStatistics result = gradebook.ComputeStatistics();
+            //Difficult to make assertions about floating point number calculations
+            Assert.Equal(85.1666666667f, result.AverageGrade);
+        }
+    }
+}
---
+++ Grades.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Grades.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
